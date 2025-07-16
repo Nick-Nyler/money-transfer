198345876bbb11d9ed97bc269f10b94997d18a6c
HanadAlly <allyhanad404@gmail.com>
2025-07-16 11:31:43 +0300

    commit changes on models.py

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,12 @@ class User(db.Model):
     email = db.Column(db.String(128), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+
+wallets = db.relationship('Wallet', backref='owner', lazy=True)
+beneficiaries = db.relationship('Beneficiary', backref='owner', lazy=True)
+
+def set_password(self, pw):
+    self.password_hash = generate_password_hash(pw)
+
+def check_password(self, pw):
+    return check_password_hash(self.password_hash, pw)
