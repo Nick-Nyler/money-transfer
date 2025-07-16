3f858d946771bbea2f04011aa75f178de48a8913
HanadAlly <allyhanad404@gmail.com>
2025-07-16 11:30:44 +0300

    commit changes on models.py

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
