719eb94598669a1c4178ce93ba308df33f0af273
geekinryxnn <ryankiratu67@gmail.com>
2025-07-17 10:55:24 +0300

    new changes

---
+++ backend/models/user.py
@@ -0,0 +1,20 @@
+from config.db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    tablename = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    wallets = db.relationship('Wallet', backref='user', lazy=True)
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
