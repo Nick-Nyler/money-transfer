ea7218136b04074c16c753602a351faa0b4330bb
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-07-17 10:21:45 +0300

    created backend structure

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'change_this')
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'postgresql://money_user:supersecret@localhost/money_transfer'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
