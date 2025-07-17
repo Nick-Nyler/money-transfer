89290a44526c7f3cac2e7837787954b1b0cbbcee
{JosephNgugi254} <solitaryjoe069@gmail.com>
2025-07-17 14:16:59 +0300

    updated config file

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_very_secret_key_for_dev'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///moneydb.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
