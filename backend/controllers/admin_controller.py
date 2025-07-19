be84fd6b82657c7e7b0ddac3fccc6486a1510b03
Nick <nicknyler@gmail.com>
2025-07-19 11:54:16 +0300

    fixed user details in admin

--- backend/controllers/admin_controller.py
+++ backend/controllers/admin_controller.py
@@ -1,17 +1,13 @@
 from models.user import User
-from models.transaction import Transaction
 from models.wallet import Wallet
-from schemas.user_schema import users_schema, user_schema
-from schemas.transaction_schema import transactions_schema
+from models.transaction import Transaction
+from schemas.user_schema import user_schema, users_schema
 from schemas.wallet_schema import wallet_schema
+from schemas.transaction_schema import transactions_schema
 
 def get_all_users():
     users = User.query.all()
     return users_schema.dump(users)
-
-def get_all_transactions():
-    transactions = Transaction.query.order_by(Transaction.created_at.desc()).all()
-    return transactions_schema.dump(transactions)
 
 def get_user_details_for_admin(user_id):
     user = User.query.get(user_id)
@@ -25,8 +21,13 @@ def get_user_details_for_admin(user_id):
     wallet_data = wallet_schema.dump(wallet) if wallet else None
     transactions_data = transactions_schema.dump(transactions)
 
+    # Flatten user_data into the root response for frontend
     return {
-        "user": user_data,
+        **user_data,
         "wallet": wallet_data,
         "transactions": transactions_data
     }
+
+def get_all_transactions():
+    transactions = Transaction.query.order_by(Transaction.created_at.desc()).all()
+    return transactions_schema.dump(transactions)
